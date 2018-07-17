bf366df9f8bef9b8401d5f59e9c4af5e98a8276e
ELIZABETH SPENCER <erss@bu.edu>
2018-07-17 17:53:09 -0400

    fixed step size

--- bects_dynamic_network_erss.m
+++ bects_dynamic_network_erss.m
@@ -14,11 +14,11 @@ model.data = [data_left;data_right];
 % % 2. LOAD MODEL PARAMETERS
 model.sampling_frequency = 2035;
 %model.t = 1:1/model.sampling_frequency:size(model.data,2)/model.sampling_frequency;
-model.window_step = 0.5; % in seconds
+model.window_step =1;% 0.5; % in seconds
 model.window_size = 1;   % in seconds
 model.q=0.05;
-model.nsurrogates = 10000;
-
+model.nsurrogates = 100;
+model.t=time;
 % % 3. Remove artifacts
 model = remove_artifacts(model,patient_coordinates);
 % 3a. INFER NETWORK
