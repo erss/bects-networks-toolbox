ef313d379aeca0ba2b24da54894a8c97f253b6bb
ELIZABETH SPENCER <erss@bu.edu>
2019-04-24 14:37:26 -0400

    fixed analyze bects table

--- analyze_BECTS_table.m
+++ analyze_BECTS_table.m
@@ -4,8 +4,8 @@ T=T_sigma;
 T.patient_name=categorical(cellstr(T.patient_name));
 %% Initialize everything
 patient_names = unique(T.patient_name);  % Unique list of patient names
-% patient_names = {'pBECTS003','pBECTS006','pBECTS007','pBECTS013','pBECTS019',...
-% 'pBECTS020'};
+patient_names = {'pBECTS003','pBECTS006','pBECTS007','pBECTS013','pBECTS019',...
+'pBECTS020'};
 % 
 % patient_names ={'pBECTS001', 'pBECTS003', 'pBECTS004','pBECTS006','pBECTS007','pBECTS019',...
 %     'pBECTS020', 'pBECTS025', 'pBECTS027','pBECTS030',...
@@ -102,53 +102,64 @@ hc        = logical(hc);
 aBECTS    = logical(aBECTS);
 rBECTS    = logical(rBECTS);
 
-temp = char(T.patient_name);
+temp = char(patient_names);
 temp = temp(:,end-1:end);
-plabels = unique(cellstr(temp))
+plabels = unique(cellstr(temp));
+
+col = [ 0.4940 0.1840 0.5560;
+    0.6032    0.8016    0.4000;
+    0.9290    0.6940    0.1250];
+
 
 %% 1) Plot Pegboard Task Performance vs sigma coherence in PrePost CG -- RIGHT HAND ONLY
-mdl = fitglm(cohPrePost(rightHand),taskPeg(rightHand))
+%mdl = fitglm(cohPrePost(rightHand),taskPeg(rightHand))
+
+mdl = fitglm(cohPrePost,taskPeg)
 b   = mdl.Coefficients.Estimate(1);
 m   = mdl.Coefficients.Estimate(2);
 
 figure; hold on;
-plot(cohPrePost(rightHand & hc),taskPeg(rightHand & hc),'sb','MarkerSize',8)
+plot(cohPrePost(rightHand & hc),taskPeg(rightHand & hc),'s','MarkerEdgeColor',col(1,:),'MarkerSize',8,'MarkerFaceColor',col(1,:))
 
-plot(cohPrePost(rightHand & aBECTS),taskPeg(rightHand & aBECTS),'sr','MarkerSize',8)
-plot(cohPrePost(rightHand & rBECTS),taskPeg(rightHand & rBECTS),'sm','MarkerSize',8)
-plot(cohPrePost(~rightHand & hc),taskPeg(~rightHand & hc),'*b','MarkerSize',8)
-plot(cohPrePost(~rightHand & aBECTS),taskPeg(~rightHand & aBECTS),'*r','MarkerSize',8)
-plot(cohPrePost(~rightHand & rBECTS),taskPeg(~rightHand & rBECTS),'*m','MarkerSize',8)
+plot(cohPrePost(rightHand & aBECTS),taskPeg(rightHand & aBECTS),'s','MarkerEdgeColor',col(2,:),'MarkerSize',8,'MarkerFaceColor',col(2,:))
+plot(cohPrePost(rightHand & rBECTS),taskPeg(rightHand & rBECTS),'s','MarkerEdgeColor',col(3,:),'MarkerSize',8,'MarkerFaceColor',col(3,:))
+plot(cohPrePost(~rightHand & hc),taskPeg(~rightHand & hc),'*','MarkerEdgeColor',col(1,:),'MarkerSize',8,'MarkerFaceColor',col(1,:))
+plot(cohPrePost(~rightHand & aBECTS),taskPeg(~rightHand & aBECTS),'*','MarkerEdgeColor',col(2,:),'MarkerSize',8,'MarkerFaceColor',col(2,:))
+plot(cohPrePost(~rightHand & rBECTS),taskPeg(~rightHand & rBECTS),'*','MarkerEdgeColor',col(3,:),'MarkerSize',8,'MarkerFaceColor',col(3,:))
 labelpoints(cohPrePost,taskPeg,plabels)
 xaxis = min(cohPrePost):.001:max(cohPrePost);
 yaxis = b+m.*xaxis;
-plot(xaxis,yaxis,'--','LineWidth',2)
+plot(xaxis,yaxis,'--r','LineWidth',2)
 legend('Healthy RH','Active RH','Remission RH','Healthy LH','Active LH',...
-    'Remission LH',['y = ' num2str(m) 'x +' num2str(b), ', p=' num2str(mdl.Coefficients.pValue(2))])
-xlabel('Sigma Coherence in Pre Post CG')
+    'Remission LH')%,['y = ' num2str(m) 'x +' num2str(b), ', p=' num2str(mdl.Coefficients.pValue(2))])
+xlabel('Pre- to Post-Central Gyrus Coherence')
 ylabel('Time to complete task (s)')
 set(gca,'FontSize',18)
+title('Motor Task Performance vs Sigma Coherence')
 %% 2) Plot Phoneme Task Performance vs sigma coherence in Left SOZ to STL -- RIGHT HAND ONLY
-mdl=fitglm(cohLang(rightHand),taskPho(rightHand))
+%mdl=fitglm(cohLang(rightHand),taskPho(rightHand))
+mdl=fitglm(cohLang,taskPho)
+
 b=mdl.Coefficients.Estimate(1);
 m=mdl.Coefficients.Estimate(2);
 
 figure; hold on;
-plot(cohLang(rightHand & hc),taskPho(rightHand & hc),'sb','MarkerSize',8)
-plot(cohLang(rightHand & aBECTS),taskPho(rightHand & aBECTS),'sm','MarkerSize',8)
-plot(cohLang(rightHand & rBECTS),taskPho(rightHand & rBECTS),'sr','MarkerSize',8)
-plot(cohLang(~rightHand & hc),taskPho(~rightHand & hc),'*b','MarkerSize',8)
-plot(cohLang(~rightHand & aBECTS),taskPho(~rightHand & aBECTS),'*m','MarkerSize',8)
-plot(cohLang(~rightHand & rBECTS),taskPho(~rightHand & rBECTS),'*r','MarkerSize',8)
+plot(cohLang(rightHand & hc),taskPho(rightHand & hc),'s','MarkerEdgeColor',col(1,:),'MarkerSize',8,'MarkerFaceColor',col(1,:))
+plot(cohLang(rightHand & aBECTS),taskPho(rightHand & aBECTS),'s','MarkerEdgeColor',col(2,:),'MarkerSize',8,'MarkerFaceColor',col(2,:))
+plot(cohLang(rightHand & rBECTS),taskPho(rightHand & rBECTS),'s','MarkerEdgeColor',col(3,:),'MarkerSize',8,'MarkerFaceColor',col(3,:))
+plot(cohLang(~rightHand & hc),taskPho(~rightHand & hc),'*','MarkerEdgeColor',col(1,:),'MarkerSize',8,'MarkerFaceColor',col(1,:))
+plot(cohLang(~rightHand & aBECTS),taskPho(~rightHand & aBECTS),'*','MarkerEdgeColor',col(2,:),'MarkerSize',8,'MarkerFaceColor',col(2,:))
+plot(cohLang(~rightHand & rBECTS),taskPho(~rightHand & rBECTS),'*','MarkerEdgeColor',col(3,:),'MarkerSize',8,'MarkerFaceColor',col(3,:))
 xaxis = min(cohLang):.001:max(cohLang);
 yaxis = b+m.*xaxis;
-plot(xaxis,yaxis,'--','LineWidth',2)
-plot(xaxis,yaxis,'--','LineWidth',2)
+plot(xaxis,yaxis,'--r','LineWidth',2)
+plot(xaxis,yaxis,'--r','LineWidth',2)
 legend('Healthy RH','Active RH','Remission RH','Healthy LH','Active LH',...
-    'Remission LH',['y = ' num2str(m) 'x +' num2str(b), ', p=' num2str(mdl.Coefficients.pValue(2))])
-xlabel('Sigma Coherence of Left SOZ to STL')
-ylabel('Raw Phono Awareness Score')
+    'Remission LH')%,['y = ' num2str(m) 'x +' num2str(b), ', p=' num2str(mdl.Coefficients.pValue(2))])
+xlabel('Left SOZ to Superior Temporal Lobe Coherence')
+ylabel('Phonological Awareness Score')
 set(gca,'FontSize',18)
+title('Language Task Performance vs Sigma Coherence')
 
 
 %% 3) 
@@ -156,57 +167,61 @@ set(gca,'FontSize',18)
 figure; 
 subplot 121
 barplot({'Active','Remission','Healthy'},cohPrePost(aBECTS),cohPrePost(rBECTS),cohPrePost(hc))
-ylabel(' Sigma Coherence in Pre Post CG')
+ylabel('Beta Coherence in Motor Network')
 axis square
-
+set(gca,'FontSize',18)
 subplot 122
 barplot({'Active','Remission','Healthy'},taskPeg(aBECTS),taskPeg(rBECTS),taskPeg(hc))
-ylabel('Pegboard Task Perforamce (seconds)')
+ylabel('Pegboard Time to Complete (seconds)')
 axis square
-
+set(gca,'FontSize',18)
 
 figure;
 subplot 121
 barplot({'Active','Remission','Healthy'},cohLang(aBECTS),cohLang(rBECTS),cohLang(hc))
-ylabel('Sigma Coherence in Left SOZ to left STL')
+ylabel('Beta Coherence in Language Network')
 axis square
-
+set(gca,'FontSize',18)
 subplot 122
 barplot({'Active','Remission','Healthy'},taskPho(aBECTS),taskPho(rBECTS),taskPho(hc))
-ylabel('Phonemic Awareness Task Perforamce Score')
+ylabel('Phonological Awareness Score')
 axis square
-
+set(gca,'FontSize',18)
+%%
 figure;
 subplot 121
 barplot({'Active','Remission','Healthy'},cohSOZ(aBECTS),cohSOZ(rBECTS),cohSOZ(hc))
-ylabel('Sigma Coherence in L/R SOZ')
+ylabel('Beta Coherence in SOZ')
 axis square
-
+set(gca,'FontSize',18)
 subplot 122
 barplot({'Active','Remission','Healthy'},cohAcross(aBECTS),cohAcross(rBECTS),cohAcross(hc))
-ylabel('Sigma Coherence from L to R SOZ')
+ylabel('Beta Coherence between SOZs')
 axis square
+[t,p]=ttest2(cohSOZ(aBECTS),cohSOZ(hc))
+[t,p]=ttest2(cohAcross(aBECTS),cohAcross(hc))
 
-
+set(gca,'FontSize',18)
 %%
 figure;
 barplot({'Active - Spiking','Active - Not Spiking','Healthy'},...
     spiking,nonspiking,hcsoz );
-ylabel('Sigma Coherence in SOZ')
+ylabel('Beta Coherence in SOZ')
 axis square
-
+set(gca,'FontSize',18)
 
 %%
 figure; hold on;
-plot(age(males & hc),cohSOZ(males & hc),'sb','MarkerSize',8);
-plot(age(males & aBECTS),cohSOZ(males & aBECTS),'sr','MarkerSize',8);
-plot(age(males & rBECTS),cohSOZ(males & rBECTS),'sm','MarkerSize',8);
-plot(age(~males & hc),cohSOZ(~males & hc),'*b','MarkerSize',8);
-plot(age(~males & aBECTS),cohSOZ(~males & aBECTS),'*r','MarkerSize',8);
-plot(age(~males & rBECTS),cohSOZ(~males & rBECTS),'*m','MarkerSize',8);
+plot(age(males & hc),cohSOZ(males & hc),'s','MarkerEdgeColor',col(1,:),'MarkerSize',8,'MarkerFaceColor',col(1,:));
+plot(age(males & aBECTS),cohSOZ(males & aBECTS),'s','MarkerEdgeColor',col(2,:),'MarkerSize',8,'MarkerFaceColor',col(2,:));
+plot(age(males & rBECTS),cohSOZ(males & rBECTS),'s','MarkerEdgeColor',col(3,:),'MarkerSize',8,'MarkerFaceColor',col(3,:));
+plot(age(~males & hc),cohSOZ(~males & hc),'*','MarkerEdgeColor',col(1,:),'MarkerSize',8,'MarkerFaceColor',col(1,:));
+plot(age(~males & aBECTS),cohSOZ(~males & aBECTS),'*','MarkerEdgeColor',col(2,:),'MarkerSize',8,'MarkerFaceColor',col(2,:));
+plot(age(~males & rBECTS),cohSOZ(~males & rBECTS),'*','MarkerEdgeColor',col(3,:),'MarkerSize',8,'MarkerFaceColor',col(3,:));
+
 legend('Healthy M', 'Active M', 'Remission M','Healthy F','Active F','Remission F')
-xlabel('age')
-ylabel('sigma coherence in L/R SOZ')
+xlabel('Age')
+ylabel('Beta Coherence in SOZ')
 set(gca,'FontSize',18)
 
 %%
@@ -220,17 +235,21 @@ figure; hold on;
 plot(cohPrePost( hc),taskPeg( hc),'sb','MarkerSize',8)
 plot(cohPrePost(affectedAndDom==1 & aBECTS),taskPeg(affectedAndDom==1 & aBECTS),'sr','MarkerSize',8)
 plot(cohPrePost(affectedAndDom==1 & rBECTS),taskPeg(affectedAndDom==1 & rBECTS),'sm','MarkerSize',8)
-plot(cohPrePost(affectedAndDom==0& aBECTS),taskPeg(affectedAndDom==0 & aBECTS),'*r','MarkerSize',8)
+plot(cohPrePost(affectedAndDom==0 & aBECTS),taskPeg(affectedAndDom==0 & aBECTS),'*r','MarkerSize',8)
 plot(cohPrePost(affectedAndDom==0 & rBECTS),taskPeg(affectedAndDom==0 & rBECTS),'*m','MarkerSize',8)
-plot(cohPrePost(isnan(affectedAndDom) & rBECTS),taskPeg(isnan(affectedAndDom) & rBECTS),'*c ','MarkerSize',8)
 xaxis = min(cohPrePost):.001:max(cohPrePost);
 yaxis = b+m.*xaxis;
 plot(xaxis,yaxis,'--','LineWidth',2)
 plot(xaxis,yaxis,'--','LineWidth',2)
-legend('Healthy','Active Ips','Remission Ips','Active Contr','Remission Contr',...
-    'Not reported Remission',['y = ' num2str(m) 'x +' num2str(b), 'p=' num2str(mdl.Coefficients.pValue(2))])
-
+legend('Healthy','Active Affected','Remission Affected','Active Not Affected','Remission Not Affected',... 
+    ['y = ' num2str(m) 'x +' num2str(b), 'p=' num2str(mdl.Coefficients.pValue(2))])
+labelpoints(cohPrePost,taskPeg,plabels)
 xlabel('Sigma Coherence in Pre Post CG')
 ylabel('Time to complete task (s)')
 set(gca,'FontSize',18)
 %%
+figure;
+barplot({'Active Affected','Active Not Affected','Remission Affected','Remission Not Affected'},taskPeg(affectedAndDom==1 & aBECTS),...
+   taskPeg(affectedAndDom==0 & aBECTS), taskPeg(affectedAndDom==1 & rBECTS),taskPeg(affectedAndDom== 0& rBECTS) )
+
+
